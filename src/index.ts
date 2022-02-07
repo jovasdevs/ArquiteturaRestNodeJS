d2a142144a67c9ed35c222a4035ff29c5f0da7db
jovasdevs <jovasdevs@gmail.com>
2022-02-06 22:52:42 -0300

    Adicionando o typescript e incrementando  a forma de desenvolver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Bora nessa bagaça!!!")
+import express, {Request, Response, NextFunction}  from "express";
+
+const app = express();
+
+app.get('/status', (req:Request , res: Response, next:NextFunction ) => {
+        res.status(200).send({foo: 'Meu dia vai chegar!!!'});
+});
+
+app.listen(3000, () => {
+    console.log('aplicação executando na porta 3000!');
+});
