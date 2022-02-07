9fa833d7e35a87e9bc63fa5ec34470ec0bb448ee
jovasdevs <jovasdevs@gmail.com>
2022-02-07 10:36:01 -0300

    CRUD Finalizado

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Esplorando o Estilo Arquitetural Rest com Node.js'.'
+# Explorando o Estilo Arquitetural Rest com Node.js!!!
+
+
+## CRUD FINALIZADO
+
+### Usuários
+
+* GET /users
+* GET /users/:uuid
+* POST /users
+* PUT /users/:uuid
+* DELETE /users/:uuid
+
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-import express, {Request, Response, NextFunction}  from "express";
+import express from "express";
+import statusRoute from "./routes/status.route";
+import usersRoute from './routes/users.route';
 
 const app = express();
 
-app.get('/status', (req:Request , res: Response, next:NextFunction ) => {
-        res.status(200).send({foo: 'Meu dia vai chegar!!!'});
-});
+// Config. da aplicação
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
+// Config. das Rotas
+app.use(usersRoute);
+
+app.use(statusRoute);
+
+// Inicialização do servidor
 app.listen(3000, () => {
     console.log('aplicação executando na porta 3000!');
 });
