8b3af94d74cea2fbf3ac5a276d1446cf3296fbf5
jovasdevs <jovasdevs@gmail.com>
2022-02-08 12:28:23 -0300

    Adicionado Conexão com Postgress e funções de manipulação da tabela de usuários

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,36 +1,49 @@
 import { Router, Request, Response, NextFunction } from "express";
 import {StatusCodes} from 'http-status-codes';
+import userRepository from '../repositories/user.repositories';
+import DatabaseError from '../models/erros/database.error.model'
 
 const usersRoute = Router();
 
-usersRoute.get('/users', (req:Request , res: Response, next:NextFunction ) => {
-    const users = [{userName: 'Jovas'}];
+usersRoute.get('/users', async (req:Request , res: Response, next:NextFunction ) => {
+    const users = await userRepository.findAllUsers();
     res.status(StatusCodes.OK).send(users);
 
 });
-usersRoute.get('/users/:uuid', (req:Request<{uuid: string}> , res: Response, next:NextFunction ) => {
-    const uuid = req.params.uuid;
-    res.status(StatusCodes.OK).send({uuid});
+usersRoute.get('/users/:uuid', async (req:Request<{uuid: string}> , res: Response, next:NextFunction ) => {
+    try {
+        const uuid = req.params.uuid;
+    const user = await userRepository.findById(uuid);
+    res.status(StatusCodes.OK).send(user);
+    } catch (error) {
+        next(error);        
+    }
+    
 
 });
 
-usersRoute.post('/users',(req:Request, res: Response, next:NextFunction ) => {
+usersRoute.post('/users', async (req:Request, res: Response, next:NextFunction ) => {
     const newUser = req.body;
-    console.log(req.body);
-    res.status(StatusCodes.CREATED).send(newUser);
+    const uuid = await userRepository.create(newUser);
+    res.status(StatusCodes.CREATED).send(uuid);
     
 });
 
-usersRoute.put('/users/:uuid', (req:Request<{uuid: string}> , res: Response, next:NextFunction ) => {
+usersRoute.put('/users/:uuid', async (req:Request<{uuid: string}> , res: Response, next:NextFunction ) => {
     const uuid = req.params.uuid;
     // Economiza um get para fazer a busca
     const modifiedUser = req.body;
     modifiedUser.uuid = uuid;
+
+    await userRepository.update(modifiedUser);
     
-    res.status(StatusCodes.OK).send(modifiedUser);
+    res.status(StatusCodes.OK).send();
 });
 
-usersRoute.delete('/users/:uuid', (req:Request<{uuid: string}> , res: Response, next:NextFunction) => {
+usersRoute.delete('/users/:uuid', async (req:Request<{uuid: string}> , res: Response, next:NextFunction) => {
+    const uuid = req.params.uuid;
+    await userRepository.remove(uuid)
+    
     res.sendStatus(StatusCodes.OK);
 
 });
